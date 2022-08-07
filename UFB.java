d9e754f362ca30f51b46908c17751bc9b35d9b70
csg <jumperbotyt@gmail.com>
2022-08-07 18:45:43 +0800

    Reverting This Anytime Soon...

--- UFB.java
+++ UFB.java
@@ -12,15 +12,9 @@ import java.util.ArrayList;
 class UFB{
 	final static char[] mem=new char[256];
 	final static int[] memInd=new int[256];
+	static byte[] bytes;
 	public static void main(final String[]a)throws Exception{
-		final byte[] data=Files.readAllBytes(Paths.get("Output.ufbb"));
-		final int[] data2=new int[data.length];
-		final StringBuilder builder=new StringBuilder();
-		for(int i=0;i<data.length;i++)
-			data2[i]=data[i]&0xff;
-		for(final int i:data2)
-			builder.append(manPadding(Integer.toBinaryString(i), 8));
-		final String bin=builder.toString();
+		bytes=Files.readAllBytes(Paths.get(a[0].trim()));
 		final String[] commands={
 			"wvar" , "nvar",
 			"trim" , "add",
@@ -31,47 +25,50 @@ class UFB{
 			"je"   , "jne",
 			"print", "read"
 		};
-		for(int i=0;i<256;i++)
-			memInd[i]=0;
+		for(int i=0;i<256;i++)memInd[i]=0;
 		mem[0]=' ';
-		for(int i=1;i<27;i++)
-			mem[i]=(char)('A'+(i-1));
-		for(int i=0;i<10;i++)
-			mem[i+27]=String.valueOf(i).charAt(0);
+		for(int i=1;i<27;i++)mem[i]=(char)('A'+(i-1));
+		for(int i=0;i<10;i++)mem[i+27]=String.valueOf(i).charAt(0);
 		mem[37]='\n';
-		final ArrayList<Integer> lines=new ArrayList<>();
-		for(int i=0;i<bin.length();i+=4){
+		final ArrayList<int[]> lines=new ArrayList<>();
+		for(;ind[0]<bytes.length-1;){
 			try{
-				if(!lines.contains(i))
-					lines.add(i);
-				System.out.println(commands[Short.parseShort(bin.substring(i, i+4), 2)]);
-				switch(commands[Short.parseShort(bin.substring(i, i+4), 2)]){
+				boolean isNewLine=true;
+				for(final int[] i:lines)
+					if(i[0]==ind[0]&&i[1]==ind[1]){
+						isNewLine=false;
+						break;
+					}
+				if(isNewLine)lines.add(new int[]{ind[0], ind[1]});
+				final String com=commands[next(4)];
+				System.out.println(com);
+				switch(com){
 					case "wvar":
-						i=wvar(bin, i+4);
+						wvar();
 						break;
 					case "nvar":
-						i=nvar(bin, i+4);
+						nvar(next(4));
 						break;
 					case "trim":
-						i=trim(bin, i+4);
+						trim();
 						break;
 					case "add":
-						i=add(bin, i+4);
+						math(0);
 						break;
 					case "sub":
-						i=sub(bin, i+4);
+						math(1);
 						break;
 					case "mul":
-						i=mul(bin, i+4);
+						math(2);
 						break;
 					case "div":
-						i=div(bin, i+4);
+						math(3);
 						break;
 					case "mod":
-						i=mod(bin, i+4);
+						math(4);
 						break;
 					case "rmod":
-						i=rmod(bin, i+4);
+						math(5);
 						break;
 					case "nop":
 						try{
@@ -87,10 +84,10 @@ class UFB{
 					case "jne":
 						break;
 					case "print":
-						i=print(bin, i+4);
+						print();
 						break;
 					case "read":
-						i=read(bin, i+4);
+						read();
 						break;
 					default:
 						break;
@@ -103,21 +100,35 @@ class UFB{
 		System.out.println(Arrays.toString(mem));
 		System.out.println(Arrays.toString(memInd));
 	}
+	static byte[] ind={0, 0};
+	private static int next(final int len){
+		if(len==4){
+			if(ind[1]==0){
+				ind[1]++;
+				return bytes[ind[0]]>>>4&0x0f;
+			}
+			ind[1]--;
+			ind[0]++;
+			return bytes[ind[0]]&0x0f;
+		}
+		return (next(4)&0x0f<<4)|(next(4)&0x0f);
+	}
 	private static String rvar(final int ind){
 		if(memInd[ind]==0)return Character.toString(mem[ind]);
 		final StringBuilder builder=new StringBuilder();
 		for(int i=ind;i<memInd[ind]+1;i++)builder.append(mem[i]);
 		return builder.toString();
 	}
-	private static int wvar(final String str, final int offset){
-		final int argCount=Short.parseShort(str.substring(offset, offset+8), 2);
-		if(argCount<1)return offset+4;
-		final int memIndex=Short.parseShort(str.substring(offset+8, offset+16), 2);
+	private static void wvar(){
+		final int argCount=next(8);
+		if(argCount<1)return;
+		final int memIndex=next(8);
+		System.out.println(argCount+"|"+memIndex);
 		final char[] temp=rvar(memIndex).toCharArray();
 		int curInd=memIndex;
-		nvar(str, offset+8);
+		nvar(memIndex);
 		for(int i=0;i<argCount-1;i++){
-			final int ind=Short.parseShort(str.substring(offset+16+(i*8), offset+24+(i*8)), 2);
+			final int ind=next(8);
 			if(memIndex==ind){
 				for(final char c:temp){
 					mem[curInd]=c;
@@ -129,238 +140,86 @@ class UFB{
 			curInd++;
 		}
 		memInd[memIndex]=curInd-1;
-		return offset+(argCount*8)+4;
 	}
-	private static int nvar(final String str, final int offset){
-		final int ind=Short.parseShort(str.substring(offset, offset+8), 2);
-		if(memInd[ind]==0){
+	private static void nvar(final int ind){
+		if(memInd[ind]==0)
 			mem[ind]='\u0000';
-			return offset+4;
-		}
-		for(int i=ind;i<memInd[ind]+1;i++)
-			if(memInd[i]==0||i==ind)
-				mem[i]='\u0000';
+		else
+			for(int i=ind;i<memInd[ind]+1;i++)
+				if(memInd[i]==0||i==ind)
+					mem[i]='\u0000';
 		memInd[ind]=0;
-		return offset+4;
 	}
-	private static int trim(final String str, final int offset){
-		final int ind=Short.parseShort(str.substring(offset  , offset+8 ), 2);
-		final int max=Short.parseShort(str.substring(offset+8, offset+16), 2);
-		if(max==0)return nvar(str, offset)+8;
-		if(max>memInd[ind]-ind)return offset+12;
+	private static void trim(){
+		final int ind=next(8);
+		final int max=next(8);
+		if(max==0){
+			nvar(ind);
+			return;
+		}
+		if(max>memInd[ind]-ind)return;
 		final char[] temp=rvar(ind).toCharArray();
-		nvar(str, offset);
+		nvar(ind);
 		for(int i=0;i<max;i++)mem[i+ind]=temp[i];
 		memInd[ind]=ind+max-1;
-		return offset+12;
 	}
-	private static int add(final String str, final int offset){
-		final int ind1=Short.parseShort(str.substring(offset  , offset+8 ), 2);
-		final int ind2=Short.parseShort(str.substring(offset+8, offset+16), 2);
+	private static void math(final int op){
+		final int ind1=next(8);
+		final int ind2=next(8);
 		final String str1=rvar(ind1);
 		final String str2=rvar(ind2);		
-		nvar(str, offset);
+		nvar(ind1);
 		if(str1.length()<1&&str2.length()>0){
 			for(int i=0;i<str2.length();i++)mem[i+ind1]=str2.charAt(i);
 			memInd[ind1]=ind1+str2.length()-1;
-			return offset+12;
+			return;
 		}
 		if(str2.length()<1){
 			for(int i=0;i<str1.length();i++)mem[i+ind1]=str1.charAt(i);
 			memInd[ind1]=ind1+str1.length()-1;
-			return offset+12;
+			return;
+		}
+		long num1;
+		long num2;
+		try{
+			num1=Long.parseLong(str1);
+		}catch(final Exception e){
+			num1=str1.hashCode();
 		}
 		try{
-			final long num1=Long.parseLong(str1);
-			final long num2=Long.parseLong(str2);
-			final char[] out=String.valueOf(num1+num2).toCharArray();
-			for(int i=ind1;i<out.length;i++)mem[ind1+i]=out[i-ind1];
-			memInd[ind1]=ind1+out.length-1;
+			num2=Long.parseLong(str2);
 		}catch(final Exception e){
-			final long num1=str1.hashCode();
-			final long num2=str2.hashCode();
-			final char[] out=String.valueOf(num1+num2).toCharArray();
-			for(int i=ind1;i<out.length;i++)mem[ind1+i]=out[i-ind1];
-			memInd[ind1]=ind1+out.length-1;
-		}
-		return offset+12;
-	}
-	private static int sub(final String str, final int offset){
-		final int ind1=Short.parseShort(str.substring(offset  , offset+8 ), 2);
-		final int ind2=Short.parseShort(str.substring(offset+8, offset+16), 2);
-		final String str1=rvar(ind1);
-		final String str2=rvar(ind2);		
-		nvar(str, offset);
-		if(str1.length()<1&&str2.length()>0){
-			for(int i=0;i<str2.length();i++)mem[i+ind1]=str2.charAt(i);
-			memInd[ind1]=ind1+str2.length()-1;
-			return offset+12;
-		}
-		if(str2.length()<1){
-			for(int i=0;i<str1.length();i++)mem[i+ind1]=str1.charAt(i);
-			memInd[ind1]=ind1+str1.length()-1;
-			return offset+12;
+			num2=str2.hashCode();
 		}
 		try{
-			final long num1=Long.parseLong(str1);
-			final long num2=Long.parseLong(str2);
-			final char[] out=String.valueOf(num1-num2).toCharArray();
-			for(int i=ind1;i<out.length;i++)mem[ind1+i]=out[i-ind1];
+			final char[] out=String.valueOf(
+				(op==0)?num1+num2:(op==1)?num1-num2:
+				(op==2)?num1*num2:(op==3)?num1/num2:
+				(op==4)?num1%num2:(long) (num1/num2)
+			).toCharArray();
+			for(int i=0;i<out.length;i++)mem[ind1+i]=out[i];
 			memInd[ind1]=ind1+out.length-1;
-		}catch(final Exception e){
-			final long num1=str1.hashCode();
-			final long num2=str2.hashCode();
-			final char[] out=String.valueOf(num1-num2).toCharArray();
-			for(int i=ind1;i<out.length;i++)mem[ind1+i]=out[i-ind1];
-			memInd[ind1]=ind1+out.length-1;
+		}catch(final Exception e){ //Divided By Zero, Mate?
+			mem[ind1]='i';
+			memInd[ind1]=ind1;
 		}
-		return offset+12;
 	}
-	private static int mul(final String str, final int offset){
-		final int ind1=Short.parseShort(str.substring(offset  , offset+8 ), 2);
-		final int ind2=Short.parseShort(str.substring(offset+8, offset+16), 2);
-		final String str1=rvar(ind1);
-		final String str2=rvar(ind2);		
-		nvar(str, offset);
-		if(str1.length()<1&&str2.length()>0){
-			for(int i=0;i<str2.length();i++)mem[i+ind1]=str2.charAt(i);
-			memInd[ind1]=ind1+str2.length()-1;
-			return offset+12;
-		}
-		if(str2.length()<1){
-			for(int i=0;i<str1.length();i++)mem[i+ind1]=str1.charAt(i);
-			memInd[ind1]=ind1+str1.length()-1;
-			return offset+12;
-		}
-		try{
-			final long num1=Long.parseLong(str1);
-			final long num2=Long.parseLong(str2);
-			final char[] out=String.valueOf(num1*num2).toCharArray();
-			for(int i=ind1;i<out.length;i++)mem[ind1+i]=out[i-ind1];
-			memInd[ind1]=ind1+out.length-1;
-		}catch(final Exception e){
-			final long num1=str1.hashCode();
-			final long num2=str2.hashCode();
-			final char[] out=String.valueOf(num1*num2).toCharArray();
-			for(int i=ind1;i<out.length;i++)mem[ind1+i]=out[i-ind1];
-			memInd[ind1]=ind1+out.length-1;
-		}
-		return offset+12;
-	}
-	private static int div(final String str, final int offset){
-		final int ind1=Short.parseShort(str.substring(offset  , offset+8 ), 2);
-		final int ind2=Short.parseShort(str.substring(offset+8, offset+16), 2);
-		final String str1=rvar(ind1);
-		final String str2=rvar(ind2);		
-		nvar(str, offset);
-		if(str1.length()<1&&str2.length()>0){
-			for(int i=0;i<str2.length();i++)mem[i+ind1]=str2.charAt(i);
-			memInd[ind1]=ind1+str2.length()-1;
-			return offset+12;
-		}
-		if(str2.length()<1){
-			for(int i=0;i<str1.length();i++)mem[i+ind1]=str1.charAt(i);
-			memInd[ind1]=ind1+str1.length()-1;
-			return offset+12;
-		}
-		try{
-			final long num1=Long.parseLong(str1);
-			final long num2=Long.parseLong(str2);
-			final char[] out=String.valueOf(num1/num2).toCharArray();
-			for(int i=ind1;i<out.length;i++)mem[ind1+i]=out[i-ind1];
-			memInd[ind1]=ind1+out.length-1;
-		}catch(final Exception e){
-			final long num1=str1.hashCode();
-			final long num2=str2.hashCode();
-			final char[] out=String.valueOf(num1/num2).toCharArray();
-			for(int i=ind1;i<out.length;i++)mem[ind1+i]=out[i-ind1];
-			memInd[ind1]=ind1+out.length-1;
-		}
-		return offset+12;
-	}
-	private static int mod(final String str, final int offset){
-		final int ind1=Short.parseShort(str.substring(offset  , offset+8 ), 2);
-		final int ind2=Short.parseShort(str.substring(offset+8, offset+16), 2);
-		final String str1=rvar(ind1);
-		final String str2=rvar(ind2);		
-		nvar(str, offset);
-		if(str1.length()<1&&str2.length()>0){
-			for(int i=0;i<str2.length();i++)mem[i+ind1]=str2.charAt(i);
-			memInd[ind1]=ind1+str2.length()-1;
-			return offset+12;
-		}
-		if(str2.length()<1){
-			for(int i=0;i<str1.length();i++)mem[i+ind1]=str1.charAt(i);
-			memInd[ind1]=ind1+str1.length()-1;
-			return offset+12;
-		}
-		try{
-			final long num1=Long.parseLong(str1);
-			final long num2=Long.parseLong(str2);
-			final char[] out=String.valueOf(num1%num2).toCharArray();
-			for(int i=ind1;i<out.length;i++)mem[ind1+i]=out[i-ind1];
-			memInd[ind1]=ind1+out.length-1;
-		}catch(final Exception e){
-			final long num1=str1.hashCode();
-			final long num2=str2.hashCode();
-			final char[] out=String.valueOf(num1%num2).toCharArray();
-			for(int i=ind1;i<out.length;i++)mem[ind1+i]=out[i-ind1];
-			memInd[ind1]=ind1+out.length-1;
-		}
-		return offset+12;
-	}
-	private static int rmod(final String str, final int offset){
-		final int ind1=Short.parseShort(str.substring(offset  , offset+8 ), 2);
-		final int ind2=Short.parseShort(str.substring(offset+8, offset+16), 2);
-		final String str1=rvar(ind1);
-		final String str2=rvar(ind2);		
-		nvar(str, offset);
-		if(str1.length()<1&&str2.length()>0){
-			for(int i=0;i<str2.length();i++)mem[i+ind1]=str2.charAt(i);
-			memInd[ind1]=ind1+str2.length()-1;
-			return offset+12;
-		}
-		if(str2.length()<1){
-			for(int i=0;i<str1.length();i++)mem[i+ind1]=str1.charAt(i);
-			memInd[ind1]=ind1+str1.length()-1;
-			return offset+12;
-		}
-		try{
-			final long num1=Long.parseLong(str1);
-			final long num2=Long.parseLong(str2);
-			final char[] out=String.valueOf((int)(num1/num2)).toCharArray();
-			for(int i=ind1;i<out.length;i++)mem[ind1+i]=out[i-ind1];
-			memInd[ind1]=ind1+out.length-1;
-		}catch(final Exception e){
-			final long num1=str1.hashCode();
-			final long num2=str2.hashCode();
-			final char[] out=String.valueOf((int)(num1/num2)).toCharArray();
-			for(int i=ind1;i<out.length;i++)mem[ind1+i]=out[i-ind1];
-			memInd[ind1]=ind1+out.length-1;
-		}
-		return offset+12;
-	}
-	private static int print(final String str, final int offset){
-		final int argCount=Short.parseShort(str.substring(offset, offset+8 ), 2);
+	private static void print(){
+		final int argCount=next(8);
 		final StringBuilder builder=new StringBuilder();
-		for(int i=0;i<argCount;i++)
-			builder.append(
-				rvar(Short.parseShort(str.substring(offset+8+(i*8), offset+16+(i*8)), 2))
-			);
+		for(int i=0;i<argCount;i++)builder.append(rvar(next(8)));
 		System.out.print(builder.toString());
-		return offset+(argCount*8)+4;
 	}
-	private static int read(final String str, final int offset){
+	private static void read(){
 		try{
-			final int ind=Short.parseShort(str.substring(offset, offset+8), 2);
+			final int ind=next(8);
 			final BufferedReader scan=new BufferedReader(new InputStreamReader(System.in));
 			System.out.print(">");
 			final char[] in=scan.readLine().toCharArray();
-			nvar(str, offset);
+			nvar(ind);
 			for(int i=ind;i<in.length+ind;i++)mem[ind+i]=in[i-ind];
 			memInd[ind]=ind+in.length-1;
 		}catch(final Exception e){}
-		return offset+4;
 	}
 	private static String manPadding(final String str, final int i){
     final StringBuilder reverse=new StringBuilder(str).reverse();
