e4339bda8ebe5dc0e0dabb92a9ad03bf472516c4
csg <jumperbotyt@gmail.com>
2022-08-09 13:41:52 +0800

    :monocle_face: Nearly There...

--- UFB.java
+++ UFB.java
@@ -14,23 +14,23 @@ import java.util.ArrayList;
 class UFB{
 	final static char[] mem=new char[256];
 	final static int[] memInd=new int[256];
-	static short[] bytes;
+	static int[] bytes;
 	public static void main(final String[]a)throws Exception{
 		try(final FileChannel fileChannel=new RandomAccessFile(a[0], "r").getChannel()){
 			final MappedByteBuffer buffer=fileChannel.map(
 				FileChannel.MapMode.READ_ONLY, 0, fileChannel.size()
 			);
-			final byte[] tempBytes=new byte[(int)fileChannel.size()];
+			final int size=(int)fileChannel.size();
+			final byte[] tempBytes=new byte[size];
 			buffer.get(tempBytes);
-			final int size=tempBytes.length;
-			bytes=new short[size];
-			for(int i=0;i<size;i++)bytes[i]=(short)(tempBytes[i]&0xff);
+			bytes=new int[size];
+			for(int i=0;i<size;i++)bytes[i]=tempBytes[i]&0xff;
 		}
-		for(int i=0;i<256;i++)memInd[i]=0;
 		mem[0]=' ';
-		for(int i=1;i<27;i++)mem[i]=(char)('A'+(i-1));
-		for(int i=0;i<10;i++)mem[i+27]=String.valueOf(i).charAt(0);
+		for(int i=0;i<26;i++)mem[i+1]=(char)(i+65);
+		for(int i=0;i<10;i++)mem[i+27]=Character.forDigit(i, 10);
 		mem[37]='\n';
+		for(int i=38;i<256;i++)mem[i]='\u0000';
 		run();
 	}
 	public static void run()throws Exception{
@@ -74,13 +74,17 @@ class UFB{
 							Thread.sleep(10);
 						}catch(final Exception nop){}
 						break;
-					case 10:
+					case 10: // jm, jl, je, jne
+						jump(0, lines);
 						break;
 					case 11:
+						jump(1, lines);
 						break;
 					case 12:
+						jump(2, lines);
 						break;
 					case 13:
+						jump(3, lines);
 						break;
 					case 14:
 						print();
@@ -91,6 +95,9 @@ class UFB{
 					default:
 						break;
 				}
+				System.out.println(com);
+				System.out.println(Arrays.toString(mem));
+				System.out.println(Arrays.toString(memInd));
 			}catch(final Exception e){
 				throw e;
 				//System.out.println(e.toString());
@@ -100,13 +107,15 @@ class UFB{
 			if(memInd[i]!=0)
 				System.out.println("Memory Leak At Index: "+String.valueOf(i));
 		}
-		//System.out.println(Arrays.toString(mem));
-		//System.out.println(Arrays.toString(memInd));
+		System.out.println(Arrays.toString(mem));
+		System.out.println(Arrays.toString(memInd));
 	}
 	static int byteInd=0;
 	private static int next(final int len){
-		byteInd++;
-		if(len==8)return bytes[byteInd-1];
+		if(len==8){
+			byteInd++;
+			return bytes[byteInd-1];
+		}
 		return (next(8)<<8)|next(8);
 	}
 	private static char[] rvar(final int ind){
@@ -117,7 +126,6 @@ class UFB{
 	}
 	private static void wvar(){
 		final int argCount=next(8);
-		if(argCount<1)return;
 		final int memIndex=next(8);
 		final char[] temp=rvar(memIndex);
 		int curInd=memIndex;
@@ -169,6 +177,13 @@ class UFB{
 		System.arraycopy(temp, 0, mem, ind, max);
 		memInd[ind]=ind+max-1;
 	}
+	private static long toNum(final String in){
+		try{
+			return Long.parseLong(in);
+		}catch(final Exception e){
+			return in.hashCode();
+		}
+	}
 	private static void math(final int op){
 		final int ind1=next(8);
 		final int ind2=next(8);
@@ -186,18 +201,8 @@ class UFB{
 			}
 			return;
 		}
-		long num1;
-		long num2;
-		try{
-			num1=Long.parseLong(new String(str1));
-		}catch(final Exception e){
-			num1=str1.hashCode();
-		}
-		try{
-			num2=Long.parseLong(new String(str2));
-		}catch(final Exception e){
-			num2=str2.hashCode();
-		}
+		final long num1=toNum(new String(str1));
+		final long num2=toNum(new String(str2));
 		try{
 			final char[] out=String.valueOf(
 				(op==0)?num1+num2:(op==1)?num1-num2:
@@ -214,6 +219,56 @@ class UFB{
 		}catch(final Exception e){ //Divided By Zero, Mate?
 			mem[ind1]='i';
 			memInd[ind1]=ind1;
+		}
+	}
+	private static void jump(final int op, final ArrayList<Integer> lines){
+		final long arg1=toNum(new String(rvar(next(8))));
+		final long arg2=toNum(new String(rvar(next(8))));
+		if(
+			(op==0&&arg1>arg2)||
+			(op==1&&arg1<arg2)||
+			(op==2&&arg1==arg2)||
+			(op==3&&arg1!=arg2)
+		){
+			final int com=next(16);
+			if(com<lines.size()+1){
+				byteInd=lines.get(com);
+				return;
+			}
+			skip(com, lines);
+		}
+	}
+	private static void skip(final int ind, final ArrayList<Integer> lines){
+		if(ind>bytes.length){
+			byteInd=bytes.length;
+			return;
+		}
+		for(;lines.size()<ind&&byteInd<bytes.length;){
+			lines.add(byteInd);
+			final int curByte=next(8);
+			if(curByte>1){
+				if(curByte<9)
+					next(16);
+				else if(curByte>9){
+					if(curByte<14){
+						next(16);
+						next(16);
+					}else if(curByte==15)
+						next(8);
+					else{
+						final int temp=next(8);
+						for(int i=0;i<temp;i++)
+							next(8);
+						// Complicated situation.
+					}
+				}
+			}else if(curByte==1)
+				next(8);
+			else{
+				final int temp=next(8);
+				for(int i=0;i<temp;i++)
+					next(8);
+			}
 		}
 	}
 	private static void print(){
