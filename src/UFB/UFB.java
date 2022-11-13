19aaa396e86819efe9cde05b80cf74b3aa46c37c
JumperBot_ <contactjumperbot@gmail.com>
2022-11-13 12:48:27 +0000

    Version Bump!

--- src/UFB/UFB.java
+++ src/UFB/UFB.java
@@ -27,7 +27,7 @@ class UFB{
 	 * MINOR CHANGES should give new commands/major features.
 	 * PATCH CHANGES should give new flags/performance-boosts/bug-fixes/etc.
 	**/
-	final static String version_tag="v1.2.0";
+	final static String version_tag="v1.2.1";
 	//----------------------------------------------------------------------//
 	public static void main(final String[]a)throws Exception{
     final FlagManager flagManager=new FlagManager(a);
