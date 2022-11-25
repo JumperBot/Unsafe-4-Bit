d33e5ac32f596f2de13ff8d89b2b3986553fa4e6
JumperBot_ <contactjumperbot@gmail.com>
2022-11-25 13:30:47 +0800

    ✨ v1.4.1 (#18)

--- src/UFB/UFBC.java
+++ src/UFB/UFBC.java
@@ -26,6 +26,11 @@ import java.io.FileReader;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -45,6 +50,9 @@ class UFBC{
 	final Pattern comment=Pattern.compile("//.*\n*");
 	final Pattern morecom=Pattern.compile("/\\*.*?\\*/", Pattern.DOTALL);
 	final StringBuilder errors=new StringBuilder();
+  final ExecutorService threads=Executors.newFixedThreadPool(
+    Runtime.getRuntime().availableProcessors()*2
+  );
   public UFBC(){}
 	public void compile(final String fileName, final boolean recompile)throws Exception{
 		final StringBuilder inBuilder=new StringBuilder();
@@ -75,11 +83,12 @@ class UFBC{
 			).replaceAll(" ")
 		);
 		final StringBuilder warnings=new StringBuilder();
-		final ArrayList<String[]> list=new ArrayList<>();
+		final ArrayList<int[]> list=new ArrayList<>();
     final Pattern jumps=Pattern.compile("j(m|l|e|ne)", Pattern.CASE_INSENSITIVE);
     final Pattern maths=Pattern.compile("add|sub|mul|div|r*mod", Pattern.CASE_INSENSITIVE);
     final Pattern pwvar=Pattern.compile("wvar|print", Pattern.CASE_INSENSITIVE);
 		boolean cancelOptimization=false;
+    final ArrayList<Command> commands=new ArrayList<>();
 		for(final String arrTemp:arr){
       final String[] realTemp=divider.split(arrTemp);
       final ArrayList<String> tempList=new ArrayList<>();
@@ -100,53 +109,16 @@ class UFBC{
                   .append("\" Will Be Ignored For It Has No Arguments: |\n")
                   .append("            ")
                   .append(lineGen(temp));
-			}else{
-				boolean isCommand=true;
-        if(temp[0].equalsIgnoreCase("trim"))
-          checkLength(temp, 3);
-        else if(temp[0].equalsIgnoreCase("nvar")){
-          if(!checkLength(temp, 2))
-            checkIfMemSafe(temp, temp[1]);
-        }else if(temp[0].equalsIgnoreCase("read")){
-          if(!checkLength(temp, 2)){
-            checkIfMemSafe(temp, temp[1]);
-            cancelOptimization=true;
-          }
-        }else if(jumps.matcher(temp[0]).matches()){
-          if(!checkLength(temp, 4))
-            for(byte i=1;i<3;i++)checkIfMem(temp, temp[i]);
-        }else if(maths.matcher(temp[0]).matches()){
-          if(!checkLength(temp, 3)){
-            checkIfMemSafe(temp, temp[1]);
-            for(byte i=1;i<3;i++)checkIfMem(temp, temp[i]);
-          }
-        }else if(pwvar.matcher(temp[0]).matches()){
-          if(temp.length>256)
-            error(
-              realTemp, "Command", temp[0],
-              "Has Too Many Arguments",
-              lineGen(temp)
-            );
-          else{
-						if(temp[0].startsWith("w"))
-              checkIfMemSafe(temp, temp[1]);
-						for(short i=2;i<temp.length;i++)
-              checkIfMem(temp, temp[i]);
-          }
-        }else if(temp[0].equalsIgnoreCase("nop")) // One day I'll regret this.
-          checkLength(temp, 1);
-        else{
-          error(
-            temp, "Command", temp[0],
-            "Does Not Exist"
-          );
-          isCommand=false;
-				}
-				if(!temp[0].equalsIgnoreCase("nop")&&isCommand)
-          checkIfMem(temp, temp[1]);
-				list.add(temp);
-			}
+			}else
+        commands.add(Command.create(temp, realTemp, threads, jumps, maths, pwvar, unicode, binaryMap));
 		}
+    threads.shutdown();
+    threads.awaitTermination(Integer.MAX_VALUE, TimeUnit.DAYS);
+    for(final Command command:commands){
+      errors.append(command.getErrors());
+      if(command.cancelOptimization())cancelOptimization=true;
+      list.add(command.getCompiled());
+    }
 		final String ANSI_RESET="\u001B[0m";
     if(warnings.length()!=0)
       System.out.printf("%s%s%s%s\n",
@@ -217,120 +189,26 @@ class UFBC{
     put('5', 32); put('6', 33); put('7', 34); put('8', 35); put('9', 36);
 		put('\n', 37);
   }};
-  private void writeToFile(final String outName, final ArrayList<String[]> list)throws Exception{
+  private void writeToFile(final String outName, final ArrayList<int[]> list)throws Exception{
     final File outFile=new File(outName);
     outFile.createNewFile();
     try(final FileOutputStream stream=new FileOutputStream(outFile)){
-      for(int i=0;i<list.size();i++){
-        final String[] temp=list.get(i);
-        stream.write(getBin(temp[0]));
-        if(temp[0].equalsIgnoreCase("wvar")||temp[0].equalsIgnoreCase("print"))
-          stream.write(temp.length-1);
-        if(temp[0].startsWith("j")){
-          for(short j=1;j<temp.length-1;j++)
-            stream.write(Integer.parseInt(temp[j]));
-          final int line=Integer.parseInt(temp[temp.length-1]);
-          final String bin=manPadding(Integer.toBinaryString(line), 16);
-          final String bin1=bin.substring(0, 8);
-          if(!bin1.contains("0"))
-            stream.write(0);
-          else
-            stream.write(Integer.parseInt(bin1, 2));
-          stream.write(Integer.parseInt(bin.substring(8), 2));
-        }else
-          for(short j=1;j<temp.length;j++)
-            stream.write(Integer.parseInt(temp[j]));
-      }
+      for(final int[] i:list)
+        for(final int out:i)
+          stream.write(out);
     }
   }
-	private void checkIfMem(final String[] temp, final String s){
-		try{
-			if(Long.parseLong(s)>255)
-				error(
-					temp, "Memory Index", s,
-					"Is Larger Than 255 And Will Not Point To Memory"
-				);
-		}catch(final Exception e){
-			error(
-				temp, "Memory Index Expected Instead Of", s,
-				"Should Be Replaced With A Memory Index"
-			);
-		}
-	}
-	private void checkIfMemSafe(final String[] temp, final String s){
-		try{
-			if(Long.parseLong(s)<38)
-				error(
-					temp, "Memory Index", s,
-					"Endangers A Read-Only Memory Index"
-				);
-		}catch(final Exception e){
-			error(
-				temp, "Memory Index Expected Instead Of", s,
-				"Should Be Replaced With A Memory Index"
-			);
-		}
-	}
-	// false if error is not thrown. Misleading eh?
-	private boolean checkLength(final String[] temp, final int length){
-		if(temp.length!=length){
-			final String num=(length<1)?"Zero":(length==1)?"One":(length==2)?"Two":"Three";
-			error(
-				temp, "Command", temp[0],
-				String.format("Needs No Less And No More Than %s Argument To Work", num)
-			);
-			return true;
-		}
-		return false;
-	}
 	private String lineGen(final String[]temp){
 		return Arrays.toString(temp).substring(1).replace(", ", " ").replace("]", "\n\n");
 	}
-	private void error(final String[] temp, final String... in){
-    if(in.length<4)
-      errors.append("Error: |\n")
-            .append(String.format("    %s: |\n", in[0]))
-            .append(String.format("        \"%s\" %s: |\n", in[1], in[2]))
-            .append(String.format("            %s", lineGen(temp)));
-    else
-      errors.append("Error: |\n")
-            .append(String.format("    %s: |\n", in[0]))
-            .append(String.format("        \"%s\" %s: |\n", in[1], in[2]))
-            .append(String.format("            %s", convertUnicode(lineGen(temp).replace("\n\n", "\n"))))
-            .append(String.format("        %s: |\n", "Which Is When Converted"))
-            .append(String.format("            %s", in[3]));
-	}
-	private long toLongAbsolute(final char[] arr){
-		// BeCoz Long#parseLong() is slow and try-catch is expensive.
-    long result=0;
-    for(final char c:arr){
-      result+=c-48;
-      result*=10;
-    }
-    return result/10;
-	}
   final Pattern unicode=Pattern.compile("(uu|UU)(\\d{1,4})");
-  private String convertUnicode(final String in){
-    if(in.length()<2)return in;
-    String temp=in;
-    try{
-      for(final Matcher m=unicode.matcher(temp);m.find();m.reset(temp)){
-        temp=new StringBuilder(temp.substring(0, m.start()))
-        .append((char)toLongAbsolute(
-          temp.substring(m.start()+2, m.end()).toCharArray()
-        ))
-        .append(temp.substring(m.end())).toString();
-      }
-    }catch(final Exception e){}
-    return temp;
-  }
 	public static String manPadding(final String str, final int i){
     return String.format(
       new StringBuilder("%").append(i).append("s").toString(),
       str
     ).replace(" ", "0");
   }
-	final HashMap<String, Short> binaryMap=new HashMap<>(){{
+	final HashMap<String, Integer> binaryMap=new HashMap<>(){{
 		final String[] commands={
       "wvar", "nvar", "trim",
       "add", "sub", "mul", "div", "mod", "rmod",
@@ -338,11 +216,8 @@ class UFBC{
       "jm", "jl", "je", "jne",
       "print", "read"
     };
-    for(short i=0;i<commands.length;i++){
+    for(int i=0;i<commands.length;i++){
       put(commands[i], i);
     }
 	}};
-	private int getBin(final String com){
-		return binaryMap.get(com);
-	}
 }
