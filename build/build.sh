650d2a2d9ec15ae52b5d364b4a471d0044617d59
JumperBot_ <contactjumperbot@gmail.com>
2022-11-27 08:40:57 +0000

    ...

--- build/build.sh
+++ build/build.sh
@@ -7,6 +7,6 @@ echo "jar --create --file=../UFB.jar --main-class=UFB *.class"
 jar --create --file=../UFB.jar --main-class=UFB *.class
 cd ../..
 # Copy changed to codespace extensions directory
-echo "cp -r syntax-highlight/* ~/.vscode-remote/extensions/"
+echo "cp -r syntax-highlight/unsafe-4-bit ~/.vscode-remote/extensions/"
 cp -r syntax-highlight/unsafe-4-bit ~/.vscode-remote/extensions/
 cd build
