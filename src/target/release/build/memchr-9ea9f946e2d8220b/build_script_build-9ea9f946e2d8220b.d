06ead48ed1c231e2bfb97e3e21bbc5af5fff5c34
csg <jumperbotyt@gmail.com>
2022-12-14 19:43:28 +0800

    Reorganization 2

--- src/target/release/build/memchr-9ea9f946e2d8220b/build_script_build-9ea9f946e2d8220b.d
+++
@@ -1,5 +0,0 @@
-/home/csg/coding/Unsafe-4-Bit/rust/Unsafe-4-Bit/target/release/build/memchr-9ea9f946e2d8220b/build_script_build-9ea9f946e2d8220b: /home/csg/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs
-
-/home/csg/coding/Unsafe-4-Bit/rust/Unsafe-4-Bit/target/release/build/memchr-9ea9f946e2d8220b/build_script_build-9ea9f946e2d8220b.d: /home/csg/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs
-
-/home/csg/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs:
--- src/test.sh
+++ src/test.sh
@@ -1,8 +1,15 @@
-UFB_RELEASE_BIN="target/release/unsafe-4-bit"
+UFB_RELEASE_DIR="target/release"
+UFB_RELEASE_BIN_TEMP="$UFB_RELEASE_DIR/unsafe-4-bit"
+UFB_RELEASE_BIN="../build/unsafe-4-bit"
 UFB_MAIN_TEST="../../test/UFB/Main.ufb"
 
-cargo build -r && \time -v $UFB_RELEASE_BIN -c $UFB_MAIN_TEST
+cargo build -r
+mv $UFB_RELEASE_BIN_TEMP $UFB_RELEASE_BIN
+sudo find . -name "*.d" -type f –delete
+rmdir $UFB_RELEASE_DIR/examples
+rmdir $UFB_RELEASE_DIR/incremental
+\time -v $UFB_RELEASE_BIN -c $UFB_MAIN_TEST
 flamegraph --root --open -- $UFB_RELEASE_BIN -c $UFB_MAIN_TEST
 sleep 2
-rm perf.data*
-rm flamegraph.svg
+sudo rm perf.data*
+sudo rm flamegraph.svg
