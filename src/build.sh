44c460ff3d2a9643fe72234d6ad5a0f8aaaf0eb0
csg <jumperbotyt@gmail.com>
2022-12-18 09:50:04 +0800

    Simplified Binary Name

--- src/build.sh
+++ src/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 UFB_RELEASE_DIR="target/release"
-UFB_RELEASE_BIN_TEMP="$UFB_RELEASE_DIR/unsafe-4-bit"
-UFB_RELEASE_BIN="../build/unsafe-4-bit"
+UFB_RELEASE_BIN_TEMP="$UFB_RELEASE_DIR/ufb"
+UFB_RELEASE_BIN="../build/ufb"
 UFB_MAIN_TEST="../test/UFB/Main.ufb"
 
 cargo build -r
 mv -f "$UFB_RELEASE_BIN_TEMP" "$UFB_RELEASE_BIN"
 rm "$UFB_RELEASE_BIN_TEMP"
 find . -type f -name "*.d" -delete
-rmdir "$UFB_RELEASE_DIR/examples"
-rmdir "$UFB_RELEASE_DIR/incremental"
+find . -type d -empty -delete
+#rmdir "$UFB_RELEASE_DIR/examples"
+#rmdir "$UFB_RELEASE_DIR/incremental"
