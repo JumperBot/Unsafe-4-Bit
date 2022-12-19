9f094249b1672d52f28911ee2b7dbcbeea9f7de7
csg <jumperbotyt@gmail.com>
2022-12-19 19:20:47 +0800

    Better Builds

--- src/build.sh
+++ src/build.sh
@@ -4,10 +4,12 @@ UFB_RELEASE_BIN_TEMP="$UFB_RELEASE_DIR/ufb"
 UFB_RELEASE_BIN="../build/ufb"
 UFB_MAIN_TEST="../test/UFB/Main.ufb"
 
+cargo clean
 cargo build -r
 mv -f "$UFB_RELEASE_BIN_TEMP" "$UFB_RELEASE_BIN"
-rm "$UFB_RELEASE_BIN_TEMP"
-find . -type f -name "*.d" -delete
-find . -type d -empty -delete
+sudo rm target -r
+#rm "$UFB_RELEASE_BIN_TEMP"
+#find . -type f -name "*.d" -delete
+#find . -type d -empty -delete
 #rmdir "$UFB_RELEASE_DIR/examples"
 #rmdir "$UFB_RELEASE_DIR/incremental"
--- src/test_runner.sh
+++ src/test_runner.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-UFB_RELEASE_DIR="target/release"
-UFB_RELEASE_BIN_TEMP="$UFB_RELEASE_DIR/ufb"
-UFB_RELEASE_BIN="../build/ufb"
+
 UFB_MAIN_TEST="../test/UFB/Main.ufb"
+UFB_DEBUG_BIN="target/debug/ufb"
+
+cargo clean
+cargo build
 
 # https://stackoverflow.com/a/60779604/16915219
-\time -f %M "$UFB_RELEASE_BIN" "${UFB_MAIN_TEST}b"
+\time -f %M "$UFB_DEBUG_BIN" "${UFB_MAIN_TEST}b"
 echo "^ Maximum resident set size (kilobytes) ^"
 
 # https://stackoverflow.com/a/16961051/16915219
 ts=$(date +%s%N)
-"$UFB_RELEASE_BIN" "${UFB_MAIN_TEST}b"
+"$UFB_DEBUG_BIN" "${UFB_MAIN_TEST}b"
 echo "It took $((($(date +%s%N) - $ts)/1000000))ms, yo."
+
+sudo rm target -r
