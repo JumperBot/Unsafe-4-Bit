daaba58e210a8f47ea25aa675f2e0142c9101698
JumperBot_ <88363421+JumperBot@users.noreply.github.com>
2022-12-23 21:03:05 +0800

    Revert "✨ v1.6.4 (#31)"
    This reverts commit c33e1a2bd10b67ea57030148d89a52c5cacc858b.

--- src/build.sh
+++ src/build.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 UFB_RELEASE_DIR="target/release"
+UFB_RELEASE_DIR_MUSL="target/aarch64-unknown-linux-musl/release"
 UFB_RELEASE_BIN_TEMP="$UFB_RELEASE_DIR/ufb"
+UFB_RELEASE_BIN_TEMP_MUSL="$UFB_RELEASE_DIR_MUSL/ufb"
 UFB_RELEASE_BIN_DIR="../build/aarch64-unknown-linux-gnu"
+UFB_RELEASE_BIN_DIR_MUSL="../build/aarch64-unknown-linux-musl"
 UFB_RELEASE_BIN="$UFB_RELEASE_BIN_DIR/ufb"
+UFB_RELEASE_BIN_MUSL="$UFB_RELEASE_BIN_DIR_MUSL/ufb"
 RUSTFLAGS="--remap-path-prefix $HOME=~"
 
 rustfmt src/*
 rm $UFB_RELEASE_BIN_DIR/*
+rm $UFB_RELEASE_BIN_DIR_MUSL/*
 rm ../build/*.tar
 cargo clean
 
 cargo build -r
+cargo build -r --target=aarch64-unknown-linux-musl
 mv -f "$UFB_RELEASE_BIN_TEMP" "$UFB_RELEASE_BIN"
+mv -f "$UFB_RELEASE_BIN_TEMP_MUSL" "$UFB_RELEASE_BIN_MUSL"
 
-UFB_RELEASE_VERSION="$(cat Cargo.toml | grep -m 1 "version" | cut -d'"' -f 2)"
+UFB_RELEASE_VERSION="$(cat Cargo.toml | grep "version" | cut -d'"' -f 2)"
 tar -cf "../build/Unsafe-4-Bit_v"$UFB_RELEASE_VERSION"_aarch64-unknown-linux-gnu.tar" -C $UFB_RELEASE_BIN_DIR . --numeric-owner
+tar -cf "../build/Unsafe-4-Bit_v"$UFB_RELEASE_VERSION"_aarch64-unknown-linux-musl.tar" -C $UFB_RELEASE_BIN_DIR_MUSL . --numeric-owner
 sudo rm target -r
 
 rm Cargo.lock
